7121e1197a46ab9647bb89b8ef5ffa9a22ef4720
Aarayn10-sudo <lazyfox916@gmail.com>
2024-12-17 08:14:57 +0545

    Nexo-footwears-server

--- src/Router/webuserRouter.js
+++ src/Router/webuserRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
 import {
   createWebuserController,
   getSpecificWebuser,
   loginWebuserController,
   verifyWebuserController,
 } from "../Controller/webUserController.js";
-import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
 export const userRouter = Router();
 userRouter.route("/register").post(createWebuserController);
 userRouter.route("/verify").post(isAuthenticated, verifyWebuserController);
 userRouter.route("/user").get(isAuthenticated, getSpecificWebuser);
+
 userRouter.route("/log-in").post(loginWebuserController);
